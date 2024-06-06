61a63071315e92e3197f171ebd72ec22591ee602
Bruno Gianini <blgianini@gmail.com>
2024-06-06 16:47:59 -0300

    import @jest/globals

--- src/components/ProdutoCard/__tests__/page.test.jsx
+++ src/components/ProdutoCard/__tests__/page.test.jsx
@@ -1,4 +1,4 @@
-import { describe } from "node:test"
+import { describe } from "@jest/globals"
 import { render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { ProdutoCard } from "../ProdutoCard"
